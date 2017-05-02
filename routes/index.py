3d738756dbbd50cee6b4c79f58e5e86d125b56da
suzumiya <jrgwyz@163.com>
2017-05-02 23:51:49 +0800

    增加搜索功能。改进首页css。

--- routes/index.py
+++ routes/index.py
@@ -10,3 +10,14 @@ def index():
     u = current_user()
     ps = Product.all()
     return render_template('index.html', u=u, ps=ps)
+
+
+@main.route('/', methods=['POST'])
+def index_search():
+    u = current_user()
+    search = request.form.get('search', None)
+    if search:
+        ps = Product.find(name={'$regex': search})
+        return render_template('index.html', u=u, ps=ps)
+    else:
+        return redirect(url_for('index.index'))
