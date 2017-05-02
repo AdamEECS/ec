75c87ab1e58fff9c7e3b02a988a85bbfaafbf0d7
suzumiya <jrgwyz@163.com>
2017-05-02 23:57:02 +0800

    修改搜索功能，修改后不区分大小写。

--- routes/index.py
+++ routes/index.py
@@ -17,7 +17,7 @@ def index_search():
     u = current_user()
     search = request.form.get('search', None)
     if search:
-        ps = Product.find(name={'$regex': search})
+        ps = Product.find(name={'$regex': search, '$options': '$i'})
         return render_template('index.html', u=u, ps=ps)
     else:
         return redirect(url_for('index.index'))
