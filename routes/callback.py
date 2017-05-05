e3c35273a555026b712ad735fa9a8ae63c66cc40
suzumiya <jrgwyz@163.com>
2017-05-05 21:51:08 +0800

    callback测试

---
+++ routes/callback.py
@@ -0,0 +1,30 @@
+from models.user import User
+from models.product import Product
+from models.order import Order
+from routes import *
+from flask import current_app as app
+from decimal import Decimal
+import qiniu
+
+main = Blueprint('callback', __name__)
+access_key = '4inGYVb4FYcXg5V9suieWkx80yKefQphTpJ4rELS'
+secret_key = 'LqW9ei2fXQDHR9UQEg_Ay-6qff00dwTmNu9tDpLz'
+q = qiniu.Auth(access_key, secret_key)
+bucket_name = 'buy-suzumiya'
+
+
+@main.route('/callback', methods=['POST'])
+def callback():
+    form = request.form
+
+
+@main.route('/product_add', methods=['POST'])
+@admin_required
+def product_add():
+    form = request.form
+    header = request.headers
+    print(header)
+    print(form)
+    r = {"success": True}
+    return json.dumps(r)
+
